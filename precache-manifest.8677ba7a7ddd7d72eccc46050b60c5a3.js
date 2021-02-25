0f524e45d5dfd7c2dc4e4f9e6281b95fa4038d5e
Magnus Tovslid <magnus.tovslid@gmail.com>
2021-02-25 19:42:26 +0100

    Updates

---
+++ precache-manifest.8677ba7a7ddd7d72eccc46050b60c5a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5654f42539def73e2dbb52582de15e16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec9dbcd5b92609e9340e",
+    "url": "/static/css/main.c24a009d.chunk.css"
+  },
+  {
+    "revision": "0dc36b079307dbb4781d",
+    "url": "/static/js/2.dd844c44.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.dd844c44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec9dbcd5b92609e9340e",
+    "url": "/static/js/main.a1b7ef2f.chunk.js"
+  },
+  {
+    "revision": "c71dd58cd5349d4e6ab8",
+    "url": "/static/js/runtime-main.4dd858cb.js"
+  }
+]);
